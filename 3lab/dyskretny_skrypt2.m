10f44e89bab8498d1e3c33444682abc777603334
Zuzanna <zuzaxmejer@gmail.com>
2022-11-30 19:23:06 +0100

    prawie

--- 3lab/dyskretny_skrypt2.m
+++ 3lab/dyskretny_skrypt2.m
@@ -1,10 +1,13 @@
+% rysowanie zależności Q od Td
+
 clear all;
 close all;
 
 k1 = 1;
 k2 = .5;
 
-Td = [0.01, 0.05, 0.1, 0.3, 0.5, 0.8, 1];
+Td = 0.1:0.3:1.8;
+%Td = [0.05, 0.1, 0.3, 0.5, 0.8, 1];
 c = ['k', 'b', 'r', 'g', 'c', 'm', 'o'];
 
 sys = tf([k1, k2], [1, 0]);
@@ -20,6 +23,6 @@ for i = Td
      b = ab(2);
      [t] = sim('second_2');
      txt = ['Td = ', num2str(i)];
-     plot(t.tout, t.w, 'DisplayName', txt);
+     plot(t.tout, t.w1, 'DisplayName', txt);
 end
 legend show;
